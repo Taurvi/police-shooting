14d2e04ec24d16c402e3e202ee5e83682d342167
Taurvi <srimbakusumo@gmail.com>
2015-07-03 21:59:54 -0700

    Added map loading message.

--- js/map.js
+++ js/map.js
@@ -1,21 +1,22 @@
+// MapBox Token
 L.mapbox.accessToken = 'pk.eyJ1IjoidGF1cnZpIiwiYSI6IjM1ODgzNzk4YjA5ODBkMDNlMjcwNjQ3NzU5MzE2MDY1In0.0ERCQNqvljTqSGNoUzZnnA';
 
 // Function to draw your map
 var drawMap = function() {
-
-
-
     // Create map and set viewd
-    var map = L.mapbox.map('map-container', 'mapbox.dark');
-    map.setView([39.833333, -98.583333], 6);
+    var map = L.mapbox.map('map-container');
+    map.setView([39.833333, -98.583333], 5);
 
     // Create an tile layer variable using the appropriate url
-
+    var layer = L.mapbox.tileLayer('mapbox.dark');
     // Add the layer to your map
+    layer.on('ready', function() {
+        $('#loading').remove();
+        this.addTo(map);
+    })
 
 
     // Execute your function to get data
-
 }
 
 // Function for getting data
