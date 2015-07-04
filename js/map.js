ad5e5f6f4fbd2519dfa64f640099196ba9c1e95a
Taurvi <srimbakusumo@gmail.com>
2015-07-03 20:49:46 -0700

    Setup basic functionality of the map using MapBox.

--- js/map.js
+++ js/map.js
@@ -1,32 +1,37 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoidGF1cnZpIiwiYSI6IjM1ODgzNzk4YjA5ODBkMDNlMjcwNjQ3NzU5MzE2MDY1In0.0ERCQNqvljTqSGNoUzZnnA';
+
 // Function to draw your map
-var drawMap() = function() {
+var drawMap = function() {
 
-  // Create map and set viewd
- 
 
-  // Create an tile layer variable using the appropriate url
 
-  // Add the layer to your map
- 
+    // Create map and set viewd
+    var map = L.mapbox.map('map-container', 'mapbox.dark');
+    map.setView([39.833333, -98.583333], 6);
 
-  // Execute your function to get data
- 
+    // Create an tile layer variable using the appropriate url
+
+    // Add the layer to your map
+
+
+    // Execute your function to get data
+
 }
 
 // Function for getting data
 var getData = function() {
 
-  // Execute an AJAX request to get the data in data/response.js
+    // Execute an AJAX request to get the data in data/response.js
 
 
-  // When your request is successful, call your customBuild function
+    // When your request is successful, call your customBuild function
 
 }
 
 // Do something creative with the data here!  
 var customBuild = function() {
 
-  
+
 }
 
 
