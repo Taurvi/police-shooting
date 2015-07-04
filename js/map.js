9d59f2fcffe7e760e777e77118a15816bc9763a8
Taurvi <srimbakusumo@gmail.com>
2015-07-03 23:30:15 -0700

    Added color picker.

--- js/map.js
+++ js/map.js
@@ -3,35 +3,40 @@ L.mapbox.accessToken = 'pk.eyJ1IjoidGF1cnZpIiwiYSI6IjM1ODgzNzk4YjA5ODBkMDNlMjcwN
 
 // Debug function
 var debugMode = true;
+var map;
+
+// Option chnages
+var genColor;
+
 
 var debugMsg = function(text) {
     if(debugMode)
         console.log('DEBUG: ' + text);
-}
+};
 var resizeMapHeight = function() {
     var setHeight = $(window).innerHeight() - 20;
     debugMsg('resizeMapHeight called! Height is now: ' + setHeight);
     $('#map-container').css('height', setHeight);
-}
+};
 
 // On window resize, resize container
 $(window).resize(function() {
-    debugMsg('Window has been resized!')
+    debugMsg('Window has been resized!');
     resizeMapHeight();
 });
 
 // Function to draw your map
 var drawMap = function() {
-    debugMsg('drawMap called!')
+    debugMsg('drawMap called!');
     // Create map and set viewd
-    var map = L.mapbox.map('map-container');
+    map = L.mapbox.map('map-container');
     map.setView([39.833333, -97.583333], 4);
 
     // Create an tile layer variable using the appropriate url
     var layer = L.mapbox.tileLayer('mapbox.dark');
     // Add the layer to your map after map data has been received
     layer.on('ready', function() {
-        debugMsg('Map layers finished loading.')
+        debugMsg('Map layers finished loading.');
         // Removes the loading message
         $('#map-loading').remove();
         // Adds to map
@@ -39,31 +44,50 @@ var drawMap = function() {
 
         getData();
     });
-}
+};
 
 // Function for getting data
 var getData = function() {
-    debugMsg('getData called!')
+    debugMsg('getData called!');
+    $('#data-loading').css('display', 'initial');
+    $('#data-options').css('display', 'none');
     // Execute an AJAX request to get the data in data/response.js
     $.ajax({
         url: 'data/response.json',
         type: 'get',
-        success: function(){
+        success: function(dat){
             $('#data-loading').css('display', 'none');
             $('#data-options').css('display', 'initial');
+
+            $('#data-generic').change(function() {
+                genColor = $('#data-generic').val();
+                debugMsg('Color: ' + genColor);
+                getData();
+            });
             debugMsg('Data finished loading.')
+            customBuild(dat);
         },
         dataType: 'json'
     });
 
     // When your request is successful, call your customBuild function
 
-}
+};
 
 // Do something creative with the data here!  
-var customBuild = function() {
-    debugMsg('customBuild called!')
+var customBuild = function(data) {
+    debugMsg('customBuild called!');
+    data.map(function(entry) {
+        if (!genColor)
+            genColor = '#FF0000';
+        var marker = new L.circle([entry.lat, entry.lng], 100, {
+            color: genColor,
+            opacity: 0.5
+        });
+        marker.addTo(map);
+    });
+};
 
-}
+
 
 
