33b0c29c43dfddfce4b796896128fbe04f682c74
Taurvi <srimbakusumo@gmail.com>
2015-07-03 22:39:09 -0700

    Added data options elements. Created function to import data.

--- js/map.js
+++ js/map.js
@@ -1,19 +1,28 @@
 // MapBox Token
 L.mapbox.accessToken = 'pk.eyJ1IjoidGF1cnZpIiwiYSI6IjM1ODgzNzk4YjA5ODBkMDNlMjcwNjQ3NzU5MzE2MDY1In0.0ERCQNqvljTqSGNoUzZnnA';
 
+// Debug function
+var debugMode = true;
+
+var debugMsg = function(text) {
+    if(debugMode)
+        console.log('DEBUG: ' + text);
+}
 var resizeMapHeight = function() {
     var setHeight = $(window).innerHeight() - 20;
-    console.log(setHeight);
+    debugMsg('resizeMapHeight called! Height is now: ' + setHeight);
     $('#map-container').css('height', setHeight);
 }
 
 // On window resize, resize container
 $(window).resize(function() {
+    debugMsg('Window has been resized!')
     resizeMapHeight();
 });
 
 // Function to draw your map
 var drawMap = function() {
+    debugMsg('drawMap called!')
     // Create map and set viewd
     var map = L.mapbox.map('map-container');
     map.setView([39.833333, -97.583333], 4);
@@ -22,18 +31,30 @@ var drawMap = function() {
     var layer = L.mapbox.tileLayer('mapbox.dark');
     // Add the layer to your map after map data has been received
     layer.on('ready', function() {
+        debugMsg('Map layers finished loading.')
         // Removes the loading message
-        $('#loading').remove();
+        $('#map-loading').remove();
         // Adds to map
         this.addTo(map);
+
+        getData();
     });
 }
 
 // Function for getting data
 var getData = function() {
-
+    debugMsg('getData called!')
     // Execute an AJAX request to get the data in data/response.js
-
+    $.ajax({
+        url: 'data/response.json',
+        type: 'get',
+        success: function(){
+            $('#data-loading').css('display', 'none');
+            $('#data-options').css('display', 'initial');
+            debugMsg('Data finished loading.')
+        },
+        dataType: 'json'
+    });
 
     // When your request is successful, call your customBuild function
 
@@ -41,7 +62,7 @@ var getData = function() {
 
 // Do something creative with the data here!  
 var customBuild = function() {
-
+    debugMsg('customBuild called!')
 
 }
 
